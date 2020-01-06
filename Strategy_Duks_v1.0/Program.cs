45196eee3024cc1234417eb02091e23b7c8a0f29
m.karsakou <m.karsakou@gmail.com>
2020-01-06 19:46:06 +0300

    Initial commit

---
+++ Strategy_Duks_v1.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategy_Duks_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
